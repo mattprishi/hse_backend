d45dd4f76fec67a90d2d86ce67b3ddfacb913734
cnrprod1755265568-user-102440 <cnrprod1755265568-user-102440>
2026-01-22 00:02:41 +0300

    fix tests

--- hw1/errors.py
+++ hw1/errors.py
@@ -1,4 +1,4 @@
 class PredictionError(Exception):
-    pass
+    ...
 
 
--- hw1/tests/conftest.py
+++ hw1/tests/conftest.py
@@ -2,6 +2,10 @@ from typing import Generator
 import pytest
 from fastapi.testclient import TestClient
 
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from main import app
 
 
